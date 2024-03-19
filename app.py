7164408929733cd413be380c43259501b925ad39
kuofunshing <163220723+kuofunshing@users.noreply.github.com>
2024-03-19 16:36:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
-
 from flask import request, abort
-from linebot import  LineBotApi, WebhookHandler
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import openai
 import os
 
+app = Flask(__name__)
+
+# 設置您的環境變數
 openai.api_key = os.getenv('OPENAI_API_KEY')
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler1 = WebhookHandler(os.getenv('CHANNEL_SECRET'))
@@ -24,19 +25,20 @@ def callback():
 
 @handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text1=event.message.text
+    text1 = event.message.text
     response = openai.ChatCompletion.create(
         messages=[
-            {"role": "user", "content": text1}
+            {"role": "user", "content": text1},
+            {"role": "user", "content": ""}  # 將 "司機" 角色視為 "user" 的特殊情況
         ],
         model="gpt-3.5-turbo-0125",
-        temperature = 0.5,
+        temperature=0.5,
     )
     try:
         ret = response['choices'][0]['message']['content'].strip()
     except:
         ret = '發生錯誤！'
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ret))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=ret))
 
 if __name__ == '__main__':
     app.run()
